b49d1190e59627e1fc9c5efafab4a37dd91450c4
Jacob Henson <Gammernut@aol.com>
2020-10-13 02:13:42 -0500

    zxc

--- program.py
+++ program.py
@@ -1,9 +1,8 @@
-
 import utill
 
 utill.print_header(code_name='FIZZ BUZZ')
 
-for x in range(1,101):
+for x in range(1, 101):
     if x % 3 == 0 and x % 5 == 0:
         print('fizz buzz')
     elif x % 3 == 0:
@@ -12,5 +11,3 @@ for x in range(1,101):
         print('buzz')
     else:
         print(x)
-
-# wow
